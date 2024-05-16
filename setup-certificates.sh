c8c826495333f9a8bf861e84cdba7cb3f3ebe568
Croway <federico.mariani.1990@gmail.com>
2024-05-16 14:10:36 +0200

    A server without spring-web

--- setup-certificates.sh
+++ setup-certificates.sh
@@ -6,6 +6,10 @@ echo cp $SERVER_JKS in ssl-server/src/main/resources
 rm ssl-server/src/main/resources/server.jks || true
 cp $SERVER_JKS ssl-server/src/main/resources/server.jks
 
+echo cp $SERVER_JKS in ssl-camel-server/src/main/resources
+rm ssl-camel-server/src/main/resources/server.jks || true
+cp $SERVER_JKS ssl-camel-server/src/main/resources/server.jks
+
 echo cp $CLIENT_JKS in ssl-server/src/main/resources
 rm ssl-client/src/main/resources/client.jks || true
 cp $CLIENT_JKS ssl-client/src/main/resources/client.jks
